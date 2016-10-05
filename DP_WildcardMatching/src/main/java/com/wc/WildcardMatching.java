d15bd92d4212974cd42150656591b3d917184f71
manibhushan <manibhushan.cs@gmail.com>
2016-10-04 18:50:58 -0700

    wildcard matching

--- DP_WildcardMatching/src/main/java/com/wc/WildcardMatching.java
+++ DP_WildcardMatching/src/main/java/com/wc/WildcardMatching.java
@@ -1,5 +1,24 @@
 package main.java.com.wc;
 
+/*
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "*") → true
+isMatch("aa", "a*") → true
+isMatch("ab", "?*") → true
+isMatch("aab", "c*a*b") → false
+*/
+
 public class WildcardMatching {
 
 	public static void main(String[] args) {
